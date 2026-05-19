13268c7244bf202d21ae52d3cc35087d4608fe98
thomasdavidch-afk <thomas.david.ch@gmail.com>
2026-05-19 12:48:15 +0200

    Finish NosMenus Page

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,9 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/nosmenus", "Nos Menus", "/pages/nosmenus.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Vite & Gourmand";
