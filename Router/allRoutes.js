667487ef7053639b3bf39597726c96533b8e3905
thomasdavidch-afk <thomas.david.ch@gmail.com>
2026-05-19 15:44:41 +0200

    Ajout Auth et Commandes

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,11 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/nosmenus", "Nos Menus", "/pages/nosmenus.html"),
     new Route("/MenuJoyeuxNoelIndien", "Menu Joyeux Noel Indien", "/pages/MenuJoyeuxNoelIndien.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon Compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Changement mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/allCommandes", "Vos commandes", "/pages/commandes/allCommandes.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
